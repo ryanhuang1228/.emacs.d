5b97b03922a067da08a694fc3e7d520b10007454
Ryan Huang <ryanhuang1228@outlook.com>
2026-02-17 01:53:29 +0800

    add ivy-posframe plugin to place the minibuffer area in the center

--- lisp/init-keybind.el
+++ lisp/init-keybind.el
@@ -24,6 +24,24 @@
 	ivy-count-format "%d/%d "
 	enable-recursive-minibuffers t
 	ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
+(use-package ivy-posframe
+  :ensure t
+  :after ivy
+  :demand
+  :config
+  ;; 设置 posframe 显示在屏幕正中央
+  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
+  ;; 可选：设置窗口大小
+  (setq ivy-posframe-parameters
+        '((left . 0.5)
+          (top . 0.3)
+          (width . 100)
+          (height . 10)))
+  ;; 禁止根据内容自动调整大小
+  (setq ivy-posframe-height 10)   ; 固定高度
+  (setq ivy-posframe-width 100)    ; 固定宽度
+  ;; 启用 ivy-posframe
+  (ivy-posframe-mode 1))
 
 (use-package counsel
   :after (ivy)
