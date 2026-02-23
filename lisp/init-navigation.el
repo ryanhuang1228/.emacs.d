406ae5663983e19d753537d275565568b6283527
Ryan Huang <ryanhuang1228@outlook.com>
2026-02-23 14:44:38 +0800

    fix up the lag in treemacs when src directory is expanded

--- lisp/init-navigation.el
+++ lisp/init-navigation.el
@@ -1,3 +1,5 @@
+;; 文件导航栏设置
+
 (use-package treemacs
   :ensure t   ; 确保通过 MELPA 安装
   :defer t    ; 延迟加载，提升 Emacs 启动速度
@@ -8,6 +10,15 @@
   (treemacs-follow-mode t)           ; 自动在侧边栏定位当前打开的文件
   (treemacs-filewatch-mode t)        ; 当文件系统有变化时自动刷新
   (treemacs-git-mode -1)
+
+  ;; 方案：将 src 的特殊图标改为普通文件夹图标
+  (with-eval-after-load 'treemacs
+    ;; 等待 treemacs 完全加载后修改图标配置
+    (let ((gui-icons (treemacs-theme->gui-icons treemacs--current-theme)))
+      ;; 将 src 的图标设置为 nil，让它们使用默认的文件夹图标
+      (ht-set! gui-icons "src-open" nil)
+      (ht-set! gui-icons "src-closed" nil)))
+
   (define-key treemacs-mode-map (kbd "C-c c f") #'treemacs-create-file)
   (define-key treemacs-mode-map (kbd "C-c c d") #'treemacs-create-dir)
   (define-key treemacs-mode-map (kbd "C-c r")   #'treemacs-rename-file)
